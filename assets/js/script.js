9b2d6ca51570c63d030d646aaf367ced092f01ac
Explosion247 <Dave.staward@gmail.com>
2025-09-07 15:22:40 +0100

    Added functions list to script js

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function rungame() {}
+function checkAnswer() {}
+function calculateCorrectAnswer() {}
+function incrementScore() {}
+function incrementWrongAnswer() {}
+function displayAdditionQuestion() {}
+function displaySubtractQuestion() {}
+function displayMultiplyQuestion() {}
